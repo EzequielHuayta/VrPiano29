32eab5f5fc23a505642578fcb8569f0282839971
EZEQUIEL LAUTARO HUAYTA ROMAY <44156990@comunidadort.edu.ar>
2020-06-08 19:41:29 -0300

    Update Key46.cs

--- New Unity Project/Assets/Scripts piano/a/Key46.cs
+++ New Unity Project/Assets/Scripts piano/a/Key46.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Key46 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+public AudioSource key46;
+private void OnMouseDown() 
+{
     {
-        
+        key46.Play();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
+}
 }
